3c1e025a346e96bfad0bb47b6d652455d7abcb60
SangKwon93 <apfhda7@gmail.com>
2022-11-04 00:00:54 +0900

    refactor : 가독성 높이는 코드로 리팩토링

--- lucky_draw_air/lucky_draw_air/spiders/spider.py
+++ lucky_draw_air/lucky_draw_air/spiders/spider.py
@@ -11,81 +11,58 @@ from fake_useragent import UserAgent
 import urllib.request
 import datetime as dt
 from datetime import datetime
-from lucky_draw_air.logerr import logerr
-
-headers = {"User-Agent" : UserAgent().chrome }
-#res = requests.get(url,headers=headers)
-
 from lucky_draw_air.items import LuckyDrawAirItem
 
+headers = {"User-Agent" : UserAgent().chrome }
 
 class Spider(scrapy.Spider):
-    name="lucky_draw_air"
+    name="luckydraw"
     allow_dimain=["https://www.luck-d.com/"]
     start_urls = ["https://www.luck-d.com/"]
+    # pip install scrapy-fake-useragent 패키지 미들웨어를 사용한다.
+    # 미들웨어 == 연결하기 직전 과정 (header 검사도 미들웨어 하나의 과정)
+    
     custom_settings = {
         'DOWNLOADER_MIDDLEWARES': {
             'scrapy.downloadermiddlewares.useragent.UserAgentMiddleware': None,
             'scrapy_fake_useragent.middleware.RandomUserAgentMiddleware': 400,
         }
     }
-
-   
-    
+    # 상세 페이지 접속
     def parse(self, response):
-        url_list=[]
-        soup = BeautifulSoup(response.text)
-        tm1 = soup.select('div.agent_site_info > h5 ')
-        
-        for tt in tm1:
-            url_list.append(tt.select_one('a')['href'])
-            
-        for ttt in url_list:
-            link = response.urljoin(ttt)
+        url_list= response.xpath('//*[@id="ogIntro"]/div[2]/div/div/div/div[2]/h5/a/@href').extract()
+        for link in url_list:
+            link = response.urljoin(link)
             yield scrapy.Request(link,callback=self.detail_info)
-        
-        # 오류 시 확인 할 수 있도록 기록을 남기려다가 실패 -> 추후 수정 필요
-        # if url_list is not None:
-        #     #logerr({"name": "k"})
-        #     logerr({"date": "신규 제품 없습니다."})
-        #     self.exitcode = 1
-        #      # 강제종료코드
 
-        # logerr({"date": "신규 제품 있습니다."})
-        
-        # 신규 등록 발매 정보만 가져오기
-        # url_list= response.xpath('//*[@id="ogIntro"]/div[2]/div/div/div/div[2]/h5/a/@href').extract()
-        # for link in url_list:
-        #     link = response.urljoin(link)
-        #     yield scrapy.Request(link,callback=self.detail_info)
-        
+    # 상세 페이지 수집
     def detail_info(self,response):
         item = LuckyDrawAirItem()
-        soup = BeautifulSoup(response.text)
-        global price
+        
+        # 가격
         price = response.xpath('//*[@id="ogIntro"]/div[3]/div[1]/label[4]/span/text()').extract_first()
-        global title
+        
+        # 제품명
         title = response.xpath('//*[@id="ogIntro"]/h5/text()').extract_first()
         
-        # 가격이 있으면서 제품명에 대문자 Jordan과 소문자 jordan인 경우
-         
-        if price != '-' and ("Jordan" in title or 'jordan' in title) :
-        # if price != '-' :
-            
-            release = response.xpath('//*[@id="ogIntro"]/div[3]/div[1]/label[3]/span/text()').extract_first() 
+        # 일자
+        release = response.xpath('//*[@id="ogIntro"]/div[3]/div[1]/label[3]/span/text()').extract_first() 
         
-      
-            # 월과 일이 있는 경우 현재일자와 비교
+        # 가격이 없고 조던이 있는 제품명만 추출
+        if price != '-' and ("Jordan" in title or 'jordan' in title) :    
+            
+            # 월,일이 있는 경우만 수집(년도는 다 있음)
             if '월' in release and '일' in release:
-                global now
-                now = dt.datetime.today()
-                global lucky       
-                lucky= dt.datetime.strptime(release,'%Y년 %m월 %d일')
-                if now < lucky:
-                    #lucky= dt.datetime.strptime(release,'%Y년 %m월 %d일') 
-                    #now = dt.datetime.today()    
-                    item = LuckyDrawAirItem()  
-                    soup = BeautifulSoup(response.text)
+                
+                # 현재일자 
+                now_date = dt.datetime.today()
+                
+                # 새 상품 발매일자
+                item_date= dt.datetime.strptime(release,'%Y년 %m월 %d일')
+                
+                # 새 상품만 추출(데이터 확인해보니 재발매되는 제품들도 존재)
+                if now_date < item_date:
+                    item = LuckyDrawAirItem() 
                     item["code"]=response.xpath('//*[@id="ogIntro"]/div[3]/div[1]/label[2]/span/text()').extract_first()
                     item["_id"]=response.xpath('//*[@id="ogIntro"]/h5/text()').extract_first()                     
                     item["release_date"]=response.xpath('//*[@id="ogIntro"]/div[3]/div[1]/label[3]/span/text()').extract_first()                           
@@ -96,13 +73,4 @@ class Spider(scrapy.Spider):
                     item['img_url_4']=response.xpath('//*[@id="product-carousel"]/div/div[4]/img/@src').extract_first()
                     
                     yield item
-        #item["name_ko"]=response.xpath('//*[@id="ogIntro"]/h3/text()').extract_first()
-        # item["prod_code"]=response.xpath('//*[@id="ogIntro"]/div[3]/div[1]/label[2]/span/text()').extract_first()
-        # item["name_en"]=response.xpath('//*[@id="ogIntro"]/h5/text()').extract_first()
-        # item["release_date"]=response.xpath('//*[@id="ogIntro"]/div[3]/div[1]/label[3]/span/text()').extract_first()
-        # item["price"] =response.xpath('//*[@id="ogIntro"]/div[3]/div[1]/label[4]/span/text()').extract_first()
-        # item['img_url']=soup.select_one('#product-carousel > div > div:nth-child(3) > img')['src']
-        # url_temp= item['img_url']
-        # full_name= item["name_en"]+'.jpg'
-        # item['img']= urllib.request.urlretrieve(url_temp, full_name)
-    
+ 

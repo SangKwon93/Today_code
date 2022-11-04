213f143d555c4fb8ecba2325dcb112d20d1fd210
SangKwon93 <apfhda7@gmail.com>
2022-11-04 22:43:17 +0900

    refactor : DB 접속정보 노출을 방지하고자 환경변수로 설정하여 작성

--- lucky_draw_air/lucky_draw_air/pipelines.py
+++ lucky_draw_air/lucky_draw_air/pipelines.py
@@ -7,18 +7,20 @@
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
 import pymongo
+import os
 
+# 환경변수 설정
+USER = os.environ['user']
+PW = os.environ['pw']
 
-
-class LuckydrawPipeline:
+class LuckyDrawAirPipeline:
   def process_item(self, item, spider):
-      user = 'psk'
-      pw = '1234'
-      host = 'ec2-3-39-24-39.ap-northeast-2.compute.amazonaws.com'
-      client = pymongo.MongoClient(f'mongodb://{user}:{pw}@{host}:27017/admin')
+
+      host = 'ec2-3-34-178-160.ap-northeast-2.compute.amazonaws.com'
+      client = pymongo.MongoClient(f'mongodb://{USER}:{PW}@{host}:27017/admin')
       db = client.resell
       
-     
+
       data = {"pro_code": item["code"],
               "Model_fullname": item["_id"],
                 "Release_date": item["release_date"],
